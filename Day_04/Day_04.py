e1f9ba08db9cb62aeb76f8cbb33bfe4f19f3a0c0
jaakidup <jaakidup@gmail.com>
2018-12-10 22:48:14 +0200

    The story of Part four

--- Day_04/Day_04.py
+++ Day_04/Day_04.py
@@ -16,46 +16,114 @@ class Guard:
         self.sleep_time = 0
         self.max_asleep = 0
         self.time_most_asleep = 0
+        self.total = 0
 
     def go_to_sleep(self, sleep_time):
         self.sleep_time = sleep_time
 
     def wake_up(self, up_time):
-        print("wake up")
         for time in range(up_time - self.sleep_time):
             self.times[self.sleep_time + time] += 1
         self.sleep_time = 0
-        print("recalculate sleep time")
-        # self.calculate_times()
+        self.calculate_times()
 
     def calculate_times(self):
         self.max_asleep = max(self.times)
         self.time_most_asleep = self.times.index(self.max_asleep)
+        sum = 0 
+        for i in self.times:
+            sum += i
+        self.total = sum
 
 
 class Schedule:
 
     def __init__(self):
-        self.guards = []
+        self.guards = {}
 
     def add_guard(self, guard):
-        if guard in self.guards:
-            print("guard", id, " already in schedule")
+        if self.guards.get(guard.id):
+            pass
         else:
-            print("adding guard ", guard.id, "to schedule")
-            # self.guards.append(guard)
-            self.guards.insert(int(guard.id), guard)
-            print(self.guards.index(guard))
-            print("guard", id, "successfully added to schedule at index", self.guards.index(guard))
+            self.guards[guard.id] = guard
 
     def is_guard(self, id):
-        # checks to see if guard(id) is registered on schedule
-        if len(self.guards) == 0:
-            return False
-        for guard in self.guards:
-            if guard.id == id:
-                return True
-        return False
+        return self.guards.get(id)
+
+
+# ============ #
+#   Run
+#
+
+
+input = []
+schedule = Schedule()
+
+
+f = open("./input.txt")
+
+for line in f:
+    input.append(line)
+
+input.sort()
+
+
+id = int()
+start = None
+end = None
+
+for entry in input:
+
+    if entry.split()[3][0] == "#":
+        id = int(entry.split()[3][1:])
+        schedule.add_guard(Guard(id))
+
+    if entry.split()[3] == "asleep":
+        time = int(entry.split()[1][-3:-1])
+        if schedule.is_guard(id):
+            schedule.guards[id].go_to_sleep(time)
+
+    if entry.split()[3] == "up":
+        time = int(entry.split()[1][-3:-1])
+        schedule.guards[id].wake_up(time)
+
+
+
+# ============================ #
+#   {part Un}
+# ============================ #
+
+
+who, most, what_time, laziest = 0, 0, 0, 0
+
+for id in schedule.guards:
+    if schedule.guards[id].total > most:
+        most = schedule.guards[id].total
+        laziest, time = id, schedule.guards[id].time_most_asleep
+
+print("{ part Un }", laziest * time)
+
+
+# ============================ #
+#   {part Deux}
+# ============================ #
+
+who, most, what_time = 0, 0, 0
+for id in schedule.guards:
+    max = schedule.guards.get(id).max_asleep
+    if max > most:
+        who, most, what_time = id, max, schedule.guards[id].time_most_asleep 
+
+print("{ part Deux }", who * what_time)
+
+
+
+
+
+
+
+
+
 
 
 # schedule = Schedule()
@@ -81,51 +149,3 @@ class Schedule:
 
 # print(schedule.guards[1].times)
 # print(schedule.guards[1].most_asleep())
-
-
-# ======================================================== #
-# ======================================================== #
-input = []
-schedule = Schedule()
-
-f = open("./input.txt")
-# f = open("/home/jaaki/python/adventOfCode/Day_04/input.txt")
-
-
-for line in f:
-    input.append(line)
-
-input.sort()
-
-
-id = int()
-start = None
-end = None
-
-for entry in input:
-    # print(entry.split()[3][0])
-
-    if entry.split()[3][0] == "#":
-        id = int(entry.split()[3][1:])
-        # print("guard ", id, " begins shift")
-        schedule.add_guard(Guard(id))
-
-    if entry.split()[3] == "asleep":
-        time = int(entry.split()[1][-3:-1])
-        print("guard ", id, " falls asleep at", time)
-        if schedule.is_guard(id):
-            print("guard ", id, " found")
-            print(schedule.guards[id])
-            # schedule.guards[id].go_to_sleep(time)
-        print("ok")
-        # else:
-        #     print("guard not found")
-
-    if entry.split()[3] == "up":
-        print("guard ", id, " wakes up at", time)
-        time = int(entry.split()[1][-3:-1])
-        schedule.guards[id].wake_up(time)
-
-
-# for guard in schedule.guards:
-#     print(guard)
